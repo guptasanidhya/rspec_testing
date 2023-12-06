00ebbefffbcdb52b7d67e8ecf46c1d576f8f40d9
guptasanidhya <guptasanidhya6@gmail.com>
2023-12-06 20:20:33 +0530

    now we are sending parameters along with request,this little application return an array of video game consoles and support for filtering those consoles by manufacturer, .contains_exactly() is a rspec method for testing tat certain objects are included in status_spec

--- episode 2/spec/requests/status_spec.rb
+++ episode 2/spec/requests/status_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Status requests' do
+
+"""Using a string like 'Status requests' as the description in describe is a common practice and is generally acceptable. It's a free-form description that you provide to make your test suite more readable and expressive.
+
+However, some teams and projects prefer a more structured approach to organizing their tests. Instead of using a plain string, they might use nested describe blocks to create a hierarchy of descriptions that mirror the structure of the code being tested. """
+# RSpec.describe 'Status requests' do
+RSpec.describe StatusController do
   describe 'GET /status' do
     it 'returns a status message' do
       get '/status'
       json=JSON.parse(response.body)
       expect(json['status']).to eql('ok')
-    #   expect(response.status).to eql(200)
+      expect(response.status).to eql(200)
     end
   end
 end
